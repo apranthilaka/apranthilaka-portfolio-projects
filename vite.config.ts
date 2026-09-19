4d7ad640d9d50f95184750b66e03d33d6ed6cea8
Aravinda <apranthilaka@outlook.com>
2026-09-19 15:58:34 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
