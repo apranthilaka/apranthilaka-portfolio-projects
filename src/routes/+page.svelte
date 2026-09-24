405966d334ee0697ade89a7cd33bd10cd8a06a8c
Aravinda <apranthilaka@outlook.com>
2026-09-24 08:05:49 +0530

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,282 @@
 <div
-	class="flex h-screen w-full flex-col items-center justify-center bg-slate-50 text-center text-sm"
+	class="flex min-h-screen w-full flex-col items-center overflow-x-hidden bg-slate-50 font-sans text-sm text-black antialiased"
 >
-	<div class="w-10">
-		<img src="logo.png" alt="" />
+	<div id="main-content" class="flex h-full w-full flex-col">
+		<div
+			class="bg-hero-sky flex h-screen min-h-auto w-full flex-col items-start justify-between gap-1 bg-cover bg-center p-10 text-white lg:flex-row lg:gap-20"
+		>
+			<!-- text wrapper  -->
+			<div class="flex h-full max-w-150 flex-col justify-between">
+				<div class="mb-3 flex flex-row items-center gap-1">
+					<!-- <img class="w-8" src="./assets/logo.png" alt="" /> -->
+					<span class="inline-flex items-center text-lg">
+						<span class="text-xl leading-none font-bold">✳</span>
+						<span class="leading-none">/apranthilaka</span>
+					</span>
+				</div>
+
+				<div class="text-base">
+					<span class="mb-5 inline-block text-4xl font-medium"
+						>Hey! I’m Aravinda, <br />Product Designer and Design Engineer helping companies design
+						and develop scalable SaaS, B2B, and enterprise software.
+					</span>
+					Explore my latest projects below and
+					<!-- view my portfolio on
+                        <a
+                            href="https://dribbble.com/aravinda"
+                            target="_blank"
+                            class="transition-all duration-200 underline underline-offset-2 inline-block"
+                            >Dribbble</a
+                        >
+                        or -->
+					Follow my design updates on
+
+					<a
+						href="https://x.com/apranthilaka"
+						target="_blank"
+						class="underline underline-offset-2 transition-all duration-200">Twitter/X.</a
+					>
+					<br />View my
+					<a
+						href="https://www.figma.com/proto/ctAjPvhCHPOvq0jMWllaKm/Online-CVs---2026?node-id=1-29&viewport=699%2C495%2C0.18&t=WJq0VA7SCRx5Bp8s-1&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&hide-ui=1"
+						target="_blank"
+						class="underline underline-offset-2 transition-all duration-200">CV</a
+					>
+					for work experience or find me on
+					<a
+						href="https://www.linkedin.com/in/aravindaprabath/"
+						target="_blank"
+						class="underline underline-offset-2 transition-all duration-200">LinkedIn.</a
+					>
+					<br /><br />
+
+					<!-- whatsapp telegram  -->
+				</div>
+
+				<div>
+					<div
+						class="flex w-full flex-col flex-wrap justify-start gap-2 align-top text-base lg:mt-0 lg:w-auto lg:justify-end"
+					>
+						<div>
+							Building something interesting?
+							<a
+								class="mb-1 inline-block underline underline-offset-2 transition-all duration-200"
+								target="_blank"
+								href="mailto:apranthilaka@gmail.com"
+							>
+								Let's connect!
+							</a>
+						</div>
+						<div class="flex w-full flex-row justify-start gap-2 align-top">
+							<a
+								target="_blank"
+								href="https://wa.me/94777499801"
+								class="inline-flex min-h-[40px] w-full min-w-[110px] items-center justify-center rounded-md bg-white/90 px-4 py-2 text-sm font-medium text-neutral-800 transition-all duration-200 hover:bg-white lg:w-auto"
+							>
+								WhatsApp
+							</a>
+							<a
+								href="https://t.me/apranthilaka"
+								target="_blank"
+								class="inline-flex min-h-[40px] w-full min-w-[110px] items-center justify-center rounded-md bg-white/90 px-4 py-2 text-sm font-medium text-neutral-800 transition-all duration-200 hover:bg-white lg:w-auto"
+							>
+								Telegram
+							</a>
+						</div>
+					</div>
+					<!-- whatsapp telegram  -->
+
+					<!-- available  -->
+					<!-- <div
+                            class="flex flex-row align-middle gap-2 items-center mt-5 mb-5"
+                        >
+                            <div
+                                class="text-white flex flex-row justify-center gap-1"
+                            >
+                                <i
+                                    class="text-green-700 ph-fill ph-circle text-base m-0 p-0 inline-flex justify-center items-center"
+                                ></i>
+
+                                <span>Available for Projects - </span>
+                            </div>
+                            <div
+                                class="text-sm font-normal"
+                                id="srilankaTime"
+                            ></div>
+                        </div> -->
+					<!-- available  -->
+				</div>
+			</div>
+			<!-- text wrapper  -->
+
+			<!-- APR CS -->
+			<div
+				class="mt-5 flex w-full max-w-150 flex-col flex-wrap justify-start gap-2 align-top lg:mt-0 lg:w-auto lg:justify-end"
+			>
+				<div>Code Sandbox Coming Soon!</div>
+				<div class="flex w-full flex-row justify-start gap-2 align-top lg:justify-end">
+					<a
+						target="_blank"
+						href="https://devportfolio.apranthilaka.workers.dev/"
+						class="inline-flex min-h-[40px] w-full min-w-[110px] items-center justify-center rounded-md bg-white/90 px-4 py-2 text-sm font-medium text-neutral-800 transition-all duration-200 hover:bg-white lg:w-auto"
+					>
+						View Live Prototypes
+					</a>
+				</div>
+			</div>
+			<!-- APR CS  -->
+		</div>
+
+		<!-- project grid  -->
+		<section class="w-full p-5 lg:p-10">
+			<div class="grid gap-5 lg:grid-cols-[repeat(auto-fit,minmax(auto,1fr))] lg:gap-10">
+				<!-- project 1  -->
+				<div>
+					<!-- title bar  -->
+					<div class="flex flex-col pb-5 lg:flex-row lg:justify-between">
+						<div>
+							<div class="text-base font-medium text-neutral-950">
+								Metaful - Permision manager for teams and agents
+							</div>
+							<div class="mt-1 flex items-center gap-1 text-xs text-neutral-500">
+								<img class="inline-block h-auto w-[15px]" src="./assets/wip.gif" alt="" />
+								R&D / Client / SaaS / Product Design
+							</div>
+							<div
+								class="mt-1 hidden items-center gap-1 text-right text-xs text-neutral-500 max-[1919px]:inline-flex"
+							>
+								<i class="ph-fill ph-warning-circle text-[15px] text-red-600"></i>
+								<span class="mb-[2px]">Screen is too small for Figma Prototype</span>
+							</div>
+						</div>
+
+						<div class="mt-5 hidden lg:mt-0 lg:block">
+							<a
+								target="_blank"
+								href="https://portfolio-9f4.pages.dev/"
+								class="inline-block w-auto rounded-md bg-green-600 px-4 py-2 font-normal text-white transition hover:bg-green-700"
+							>
+								HTML Prorotype
+							</a>
+						</div>
+					</div>
+					<!-- title bar  -->
+					<div class="relative aspect-4/2 overflow-hidden border border-neutral-300 bg-neutral-200">
+						<iframe
+							loading="eager"
+							fetchpriority="high"
+							class="h-full w-full border-0 border-none object-cover shadow-none"
+							src="https://embed.figma.com/proto/OiECY5HRCcUmrRlP5ZVQXa/DS3-Designs?page-id=0%3A1&node-id=2-479&viewport=-1452%2C521%2C0.18&scaling=contain&content-scaling=responsive&starting-point-node-id=2%3A399&embed-host=share"
+							allowfullscreen
+						></iframe>
+					</div>
+				</div>
+				<!-- project 1  -->
+				<div class="text-neutral-300">
+					<hr class="my-4 border-t-2 border-dotted border-gray-300" />
+				</div>
+				<!-- project 2  -->
+
+				<div>
+					<!-- title bar  -->
+					<div class="flex flex-row items-center justify-between pb-5">
+						<div>
+							<div class="text-base font-medium text-neutral-950">Obrand - Work In Progress</div>
+
+							<div class="mt-1 flex items-center gap-1 text-xs text-neutral-500">
+								<img class="inline-block h-auto w-[15px]" src="./assets/wip.gif" alt="" />
+								Work in Progress / Concept / SaaS / Product Design
+							</div>
+							<div
+								class="mt-1 hidden items-center gap-1 text-right text-xs text-neutral-500 max-[1919px]:inline-flex"
+							>
+								<i class="ph-fill ph-warning-circle text-[15px] text-red-600"></i>
+								<span class="mb-[2px]">Screen is too small for Figma Prototype</span>
+							</div>
+						</div>
+					</div>
+					<!-- title bar  -->
+					<div class="relative aspect-4/2 overflow-hidden border border-neutral-300 bg-neutral-200">
+						<iframe
+							loading="eager"
+							fetchpriority="high"
+							class="h-full w-full border-0 border-none object-cover shadow-none"
+							src="https://embed.figma.com/proto/ocyYU4bx5pCngMb18JZxqw/DS2---Designs?node-id=7583-3004&viewport=138%2C391%2C0.17&scaling=contain&content-scaling=responsive&starting-point-node-id=7002%3A249&page-id=0%3A1&embed-host=share"
+							allowfullscreen
+						></iframe>
+					</div>
+				</div>
+				<!-- project 2  -->
+
+				<div class="text-neutral-300">
+					<hr class="my-4 border-t-2 border-dotted border-gray-300" />
+				</div>
+
+				<!-- project 3  -->
+
+				<div>
+					<!-- title bar  -->
+					<div class="flex flex-row items-center justify-between pb-5">
+						<div>
+							<div class="text-base font-medium text-neutral-950">Kaleora - AI Audit</div>
+							<div class="mt-1 flex items-center gap-1 text-xs text-neutral-500">
+								<img class="inline-block h-auto w-[15px]" src="./assets/wip.gif" alt="" />
+								Work in Progress / Concept / SaaS / Product Design
+							</div>
+							<div
+								class="mt-1 hidden items-center gap-1 text-right text-xs text-neutral-500 max-[1919px]:inline-flex"
+							>
+								<i class="ph-fill ph-warning-circle text-[15px] text-red-600"></i>
+								<span class="mb-[2px]">Screen is too small for Figma Prototype</span>
+							</div>
+						</div>
+					</div>
+					<!-- title bar  -->
+					<div class="relative aspect-4/2 overflow-hidden border border-neutral-300 bg-neutral-200">
+						<iframe
+							loading="eager"
+							fetchpriority="high"
+							class="h-full w-full border-0 border-none object-cover shadow-none"
+							src="https://embed.figma.com/proto/dTfOk6yBsvtQ7ORwaXGKUy/DS1---Designs?node-id=5466-588&viewport=-435%2C515%2C0.37&scaling=contain&content-scaling=responsive&starting-point-node-id=5003%3A1502&page-id=5003%3A555&embed-host=share"
+							allowfullscreen
+						></iframe>
+					</div>
+				</div>
+				<!-- project 3  -->
+			</div>
+		</section>
+		<!-- project grid   -->
 	</div>
-	<div class="mb-2 text-lg font-medium">Launching Soon.</div>
-	<div class="max-w-110">
-		Interactive web implementations of my <a
-			target="_blank"
-			class="underline"
-			href="https://apranthilaka.com/">Figma designs</a
-		>, developed through hybrid, manual coding and AI collaboration.
+	<div class="tex-sm flex flex-col items-center justify-center gap-2 pb-10 text-neutral-500">
+		<div class="text-neutral-900">Site by @apranthilaka</div>
+		<!-- whatsapp telegram  -->
+
+		<div>
+			Building something interesting?
+			<a
+				class="mb-1 inline-block underline underline-offset-2 transition-all duration-200 hover:text-[#ea4c89]"
+				target="_blank"
+				href="mailto:apranthilaka@gmail.com"
+			>
+				Let's connect!
+			</a>
+		</div>
+		<div class="flex w-full flex-row gap-2">
+			<a
+				target="_blank"
+				href="https://wa.me/94777499801"
+				class="inline-flex w-full min-w-[110px] items-center justify-center rounded-md bg-green-600 px-4 py-2 text-sm text-white transition-all duration-200 hover:bg-green-700"
+			>
+				WhatsApp
+			</a>
+			<a
+				href="https://t.me/apranthilaka"
+				target="_blank"
+				class="inline-flex w-full min-w-[110px] items-center justify-center rounded-md bg-sky-600 px-4 py-2 text-sm text-white transition-all duration-200 hover:bg-sky-600"
+			>
+				Telegram
+			</a>
+		</div>
+		<!-- whatsapp telegram  -->
 	</div>
-	⌁
-	<div class="">Sveltekit • Shadcn • Tailwind4</div>
-	<div class="">Claude • Ollama/Qwen code</div>
-	⌁
-	<div class="text-xs text-neutral-500">Status: Migrating component architecture</div>
-	<div>/load</div>
 </div>
